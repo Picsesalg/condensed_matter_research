7cd2d8a98fa91fdeadc0c9016b473287a9be6be8
Alice <ayang36@jhu.edu>
2018-11-29 11:40:37 -0500

    Need to optimise energy calc

--- quantum_hall/sample_generate.py
+++ quantum_hall/sample_generate.py
@@ -5,6 +5,9 @@ import datetime
 import math
 
 def initLattice(ra, L, numParts):
+    now = datetime.datetime.now()
+    seed = now.hour * 3600 + now.minute * 60 + now.second
+    random.seed(seed)
     cp = 0
     while (cp < numParts):
         i = random.randint(0, L - 1)
@@ -16,26 +19,58 @@ def initLattice(ra, L, numParts):
         cp += 1
     return ra
 
-def chooseRandCoord(ra, i, j):
-    if (x1[i + charge, j] == 0):
+def chooseRandCoord(ra, i, j, L):
+    now = datetime.datetime.now()
+    seed = now.hour * 3600 + now.minute * 60 + now.second
+    random.seed(seed)
+    if (i + charge < L and ra[i + charge][j] == 0):
         s = i + charge
-           t = j
-    elif (x1[i - charge, j] == 0):
+        t = j
+    elif (i - charge >= 0 and ra[i - charge][j] == 0):
         s = i - charge
         t = j
-    elif (x1[i, j + charge] == 0):
+    elif (j + charge < L and ra[i][j + charge] == 0):
         s = i
         t = j + charge
-    elif (x1[i, j - charge] == 0):
+    elif (j - charge >= 0 and ra[i][j - charge] == 0):
         s = i
         t = j - charge
     else:
-        while (x1[i, j] != 1):
+        while (ra[i, j] != 1):
             i = random.randint(0, L - 1)
             j = random.randint(0, L - 1)
         s = i
         t = j
     return s, t
+
+def initEnergy(ra, L):
+    pivot = L // 2
+    offsetA = 0
+    offsetB = 0
+    offsetC = 0
+    offsetD = 0
+    term1 = 0
+    term2 = 0
+    distance = 0
+    for a in range(L):
+        for b in range(L):
+            if (ra[a][b] != 1):
+                continue
+            offsetA = pivot - a
+            offsetB = b - pivot
+            term2 = term2 + offsetA ** 2 + offsetB ** 2
+            for c in range(a, L):
+                for d in range(L):
+                    if (c == a and d <= b):
+                        continue
+                    if (ra[c][d] == 0):
+                        continue
+                    offsetC = pivot - c
+                    offsetD = d - pivot
+                    distance = (offsetA - offsetC) ** 2 + (offsetB - offsetD) ** 2
+                    distance = distance ** 0.5
+                    term1 = term1 - math.log(distance)
+    return term1, term2
 
 """
 Initialising random variables.
@@ -48,39 +83,46 @@ np.random.seed(seed)
 """
 Initialising variables
 """
-#Number of features on one side of the lattice structure.
+# Number of features on one side of the lattice structure.
 L = 31
-#Number of inputs.
+# Number of inputs.
 ni = L * L
-#Number of particles
+# Number of particles
 numParts = ni / 2
 itval = int(numParts / 2)
 charge = 3
 
-#Initalise all elements to 0
+# Initalise all elements to 0
 x1 = np.zeros((L, L))
 
 countParts = 0
-engy_fina_1 = 0
-engy_fina_2 = 0
+engy_init = 0
+engy_init_1 = 0
+engy_init_2 = 0
+engy_fina = 0
 
-#Initialise the lattice
+# Initialise the lattice
 x1 = initLattice(x1, L, numParts)
 
-#Monte Carlo sampling of 100 steps to converge into the phases.
+# Monte Carlo sampling of 100 steps to converge into the phases.
 for m in range (2001):
     for k in range (itval):
-#Calculate initial energy
-
-#Choose random particle to move
+# Calculate initial energy
+        engy_init_1, engy_init_2 = initEnergy(x1, L)
+        engy_init_1 = engy_init_1 * (charge ** 2)
+        engy_init_2 = (charge / 4.0) * engy_init_2
+        engy_init = engy_init_1 + engy_init_2
+# Choose random particle to move
         i = random.randint(0, L - 1)
         j = random.randint(0, L - 1)
-        while (x1[i, j] != 1):
+        while (x1[i][j] != 1):
             i = random.randint(0, L - 1)
             j = random.randint(0, L - 1)
-        x1[i, j] = 0
-        s, t = chooseRandCoord(x1, i, j)
-        x1[s, t] = 1
+        s, t = chooseRandCoord(x1, i, j, L)
+        x1[i][j] = 0
+        x1[s][t] = 1
+# Calculate new energy
+
 
 
 row = np.array([])
@@ -89,6 +131,7 @@ col = np.array([])
 for y in range(L):
     for x in range(L):
         if (x1[x][y] == 1):
+            count += 1
             row = np.append(row, x)
             col = np.append(col, y)
 
--- quantum_hall/tester.py
+++ quantum_hall/tester.py
@@ -1,8 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([1, 3, 5, 2, 1])
-y = np.array([4, 5, 4, 2, 8])
+"""
+def initEnergy(ra, L):
+    for a in range(L):
+        for b in range(L):
+            if (ra[a][b] != 1):
+                print("Zero: " + str(a) + "," + str(b))
+                continue
+            print("One: " + str(a) + "," + str(b))
+            for c in range(a, L):
+                for d in range(L):
+                    if (c == a and d <= b):
+                        print("fail1 " + str(c) + "," + str(d))
+                        continue
+                    if (ra[c][d] == 0):
+                        print("fail2 " + str(c) + "," + str(d))
+                        continue
+                    print("success"  + str(c) + "," + str(d))
 
-plt.scatter(x, y)
-plt.show()
+x1 = np.array([[0, 1, 0, 1],
+                [1, 0, 1, 0],
+                [1, 1, 0, 0],
+                [0, 0, 1, 1]])
+initEnergy(x1, 4)
+"""
+
+"""
+print(3 // 2)
+"""
+
+x = 4
+x = 4 // 3
+print(x)
